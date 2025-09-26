bd7553c10fd27905ac7e2686ab9d3d08d06f9d0c
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-09-26 15:19:22 +1200

    create 3 X 3 matrix

--- mean_var_std.py
+++ mean_var_std.py
@@ -4,5 +4,7 @@ import numpy as np
 def calculate(list):
     if len(list) != 9:
         raise ValueError('List must contain nine numbers.')
+    else:
+        matrix = np.array(list).reshape(3, 3)
 
     return calculations
