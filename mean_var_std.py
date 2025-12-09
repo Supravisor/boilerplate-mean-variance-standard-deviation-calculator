361a0d21ff7701cb1fd31d3032259018aca647f8
Supravisor <153783117+Supravisor@users.noreply.github.com>
2025-12-09 16:52:19 +1300

    add sum

--- mean_var_std.py
+++ mean_var_std.py
@@ -12,5 +12,6 @@ def calculate(list):
         "variance": [(matrix.var(axis=0).tolist()), (matrix.var(axis=1).tolist()), (matrix.flatten().var())],
         "standard deviation": [(matrix.std(axis=0).tolist()), (matrix.std(axis=1).tolist()), (matrix.flatten().std())],
         "max": [(matrix.max(axis=0).tolist()), (matrix.max(axis=1).tolist()), (matrix.flatten().max())],
-        "min": [(matrix.min(axis=0).tolist()), (matrix.min(axis=1).tolist()), (matrix.flatten().min())]
+        "min": [(matrix.min(axis=0).tolist()), (matrix.min(axis=1).tolist()), (matrix.flatten().min())],
+        "sum": [(matrix.sum(axis=0).tolist()), (matrix.sum(axis=1).tolist()), (matrix.flatten().sum())]
     }
